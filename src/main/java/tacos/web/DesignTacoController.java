9e14f4065908b9e41222f9ec8841c07448cd91a7
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-05-01 23:43:22 -0400

    order, order controler and form

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -43,6 +44,15 @@ public class DesignTacoController {
 		model.addAttribute("design", new Taco());
 		
 		return "design";	// view template name
+	}
+	
+	@PostMapping
+	public String processDesign(Taco design) {
+		// Save the taco design
+		// We'll do this in chapter 3
+		log.info("Processing design: " + design);
+		
+		return "redirect:/orders/current";
 	}
 	
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
