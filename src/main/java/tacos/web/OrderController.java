499fb53a9444f85a9fc89ad5a9add4085e3d642f
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-05-02 23:26:35 -0400

    declaring validation rules and performing validation

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,7 +1,10 @@
 package tacos.web;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +25,10 @@ public class OrderController {
 	
 	
 	@PostMapping
-	public String processOrder(Order order) {
+	public String processOrder(@Valid Order order, Errors errors) {
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
 		log.info("Order subitted: " + order);
 		return "redirect:/";
 	}
