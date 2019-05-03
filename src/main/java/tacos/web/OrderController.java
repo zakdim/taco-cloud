5b8298e60506cffb682d6bb1a0ff9b1c47151479
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-05-02 22:46:38 -0400

    2.3 processing form submission

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -3,6 +3,7 @@ package tacos.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,5 +19,11 @@ public class OrderController {
 		model.addAttribute("order", new Order());
 		return "orderForm";
 	}
-
+	
+	
+	@PostMapping
+	public String processOrder(Order order) {
+		log.info("Order subitted: " + order);
+		return "redirect:/";
+	}
 }
