b5b46abb68c2bd1b3af3e00b8058f8391f9d2fe9
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2019-05-06 14:48:59 -0400

    section 4.2.3 - ldap-backed user

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,12 +1,15 @@
 package tacos.security;
 
+import javax.naming.ldap.LdapContext;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
@@ -14,25 +17,73 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	DataSource dataSource;
 	
-	@SuppressWarnings("deprecation")
+	
+	// embadded LDAP server
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.jdbcAuthentication()
-			.dataSource(dataSource)
-			.usersByUsernameQuery(
-					"select username, password, enabled from Users " +
-					"where username=?")
-			.authoritiesByUsernameQuery(
-					"select username, authority from UserAuthorities " +
-					"where username=?")
-			.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
-		
+			.ldapAuthentication()
+			.userSearchBase("ou=people")
+			.userSearchFilter("(uid={0})")
+			.groupSearchBase("ou=groups")
+			.groupSearchFilter("member={0}")
+			.passwordCompare()
+			.passwordEncoder(new BCryptPasswordEncoder())
+			.passwordAttribute("passcode")
+			.contextSource()
+				.root("dc=tacocloud,dc=com")
+				.ldif("classpath:users.ldif");
 	}
+	
+//	// remote LDAP server
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//			.ldapAuthentication()
+//			.userSearchBase("ou=people")
+//			.userSearchFilter("(uid={0})")
+//			.groupSearchBase("ou=groups")
+//			.groupSearchFilter("member={0}")
+//			.passwordCompare()
+//			.passwordEncoder(new BCryptPasswordEncoder())
+//			.passwordAttribute("passcode")
+//			.contextSource()
+//				.url("ldap://tacocloud.com:389/dc=tacocloud,dc=com");
+//	}
+	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//			.ldapAuthentication()
+//				.userSearchBase("ou=people")
+//				.userSearchFilter("(uid={0}")
+//				.groupSearchBase("ou=groups")
+//				.groupSearchFilter("member={0}")
+//				.passwordCompare()
+//				.passwordEncoder(new BCryptPasswordEncoder())
+//				.passwordAttribute("passcode");
+//		
+//	}
+
+//	@Autowired
+//	DataSource dataSource;
+//	
+//	@SuppressWarnings("deprecation")
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//			.jdbcAuthentication()
+//			.dataSource(dataSource)
+//			.usersByUsernameQuery(
+//					"select username, password, enabled from Users " +
+//					"where username=?")
+//			.authoritiesByUsernameQuery(
+//					"select username, authority from UserAuthorities " +
+//					"where username=?")
+//			.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+//		
+//	}
 	
 //	@Override
 //	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
