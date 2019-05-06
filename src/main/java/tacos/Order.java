7f475e4b29e5ebd931619da71125e6b8ecb4ec4e
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-05-05 21:05:31 -0400

    sections 3.2.1, 3.2.2, 3.2.3

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,5 +1,17 @@
 package tacos;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -9,22 +21,32 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import lombok.Data;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
 
-	@NotBlank(message="Name is required")
-	private String name;
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private Date placedAt;
+	
+	@NotBlank(message="Delivery name is required")
+	private String deliveryName;
 	
 	@NotBlank(message = "Street is required")
-	private String street;
+	private String deliveryStreet;
 	
 	@NotBlank(message = "City is required")
-	private String city;
+	private String deliveryCity;
 	
 	@NotBlank(message = "State is required")
-	private String state;
+	private String deliveryState;
 	
 	@NotBlank(message = "Zip code is required")
-	private String zip;
+	private String deliveryZip;
 	
 	@CreditCardNumber(message = "Not a valid credit card number")
 	private String ccNumber;
@@ -35,5 +57,17 @@ public class Order {
 	
 	@Digits(integer = 3, fraction = 0, message = "Invalid CVV")
 	private String ccCVV;
-
+	
+	@ManyToMany(targetEntity = Taco.class) 
+	private List<Taco> tacos = new ArrayList<>();
+	
+	public void addDesign(Taco design) {
+		this.tacos.add(design);
+	}
+	
+	@PrePersist
+	void placedAt() {
+		this.placedAt = new Date();
+	}
+	
 }
