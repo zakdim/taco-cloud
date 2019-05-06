c26c8ea0cb7a9ffd8c66cc9c5542e2e431cb2cd1
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2019-05-06 16:23:29 -0400

    section 4.2.4 - customizing user authentication

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,23 +19,39 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
 	
-	// embadded LDAP server
+	@SuppressWarnings("deprecation")
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.ldapAuthentication()
-			.userSearchBase("ou=people")
-			.userSearchFilter("(uid={0})")
-			.groupSearchBase("ou=groups")
-			.groupSearchFilter("member={0}")
-			.passwordCompare()
-			.passwordEncoder(new BCryptPasswordEncoder())
-			.passwordAttribute("passcode")
-			.contextSource()
-				.root("dc=tacocloud,dc=com")
-				.ldif("classpath:users.ldif");
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(encoder());
 	}
+	
+	
+//	// embadded LDAP server
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//			.ldapAuthentication()
+//			.userSearchBase("ou=people")
+//			.userSearchFilter("(uid={0})")
+//			.groupSearchBase("ou=groups")
+//			.groupSearchFilter("member={0}")
+//			.passwordCompare()
+//			.passwordEncoder(new BCryptPasswordEncoder())
+//			.passwordAttribute("passcode")
+//			.contextSource()
+//				.root("dc=tacocloud,dc=com")
+//				.ldif("classpath:users.ldif");
+//	}
 	
 //	// remote LDAP server
 //	@Override
