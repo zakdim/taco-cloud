13a5cc6be23a49e2ffc0de93223b027de08113fe
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-05-08 22:54:06 -0400

    section 6.1.1 - retrieving data from server

--- tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -3,18 +3,26 @@ package tacos.web.api;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,79 +36,40 @@ import tacos.data.TacoRepository;
 import tacos.data.UserRepository;
 
 @Slf4j
-@Controller
-@RequestMapping("/design")
-@SessionAttributes("order")
+@RestController
+@RequestMapping(path = "/design",
+				produces = "application/json")
+@CrossOrigin(origins = "*")
 public class DesignTacoController {
-	
-	private final IngredientRepository ingredientRepo;
 	
 	private TacoRepository tacoRepo;
 	
-	private UserRepository userRepo;
-	
 	@Autowired
-	public DesignTacoController(
-			IngredientRepository ingredientRepo, 
-			TacoRepository tacoRepo,
-			UserRepository userRepo) {
-		this.ingredientRepo = ingredientRepo;
+	EntityLinks entityLinks;
+	
+	public DesignTacoController(TacoRepository tacoRepo) {
 		this.tacoRepo = tacoRepo;
-		this.userRepo = userRepo;
 	}
 	
-	@ModelAttribute(name = "order")
-	public Order order() {
-		return new Order();
+	@GetMapping("/recent")
+	public Iterable<Taco> recentTacos() {
+		PageRequest page = PageRequest.of(
+				0, 12, Sort.by("createdAt").descending());
+		return tacoRepo.findAll(page).getContent();
 	}
 	
-	@ModelAttribute(name = "design")
-	public Taco design() {
-		return new Taco();
-	}
-
-	@GetMapping
-	public String showDesignForm(Model model, Principal principal) {
-		log.info("   --- Designing taco");
-		List<Ingredient> ingredients = new ArrayList<>();
-		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
-		
-		Type[] types = Ingredient.Type.values();
-		for (Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(), 
-					filterByType(ingredients, type));
+//	@GetMapping("/{id}")
+//	public Taco tacoById(@PathVariable("id") Long id) {
+//		Optional<Taco> optTaco = tacoRepo.findById(id);
+//		return optTaco.orElse(null);
+//	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+		Optional<Taco> optTaco = tacoRepo.findById(id);
+		if (optTaco.isPresent()) {
+			return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
 		}
-		
-		String username = principal.getName();
-		User user = userRepo.findByUsername(username);
-		model.addAttribute("user", user);
-		
-		return "design";	// view template name
-	}
-	
-	@PostMapping
-	public String processDesign(
-			@Valid Taco taco, Errors errors,
-			@ModelAttribute Order order) {
-		
-		log.info("   --- Saving taco");
-		
-		if (errors.hasErrors()) {
-			return "design";
-		}
-		
-		Taco saved = tacoRepo.save(taco);
-		order.addDesign(saved);
-		
-		return "redirect:/orders/current";
-	}
-	
-	private List<Ingredient> filterByType(
-			List<Ingredient> ingredients, Type type) {
-		
-		return ingredients
-				.stream()
-				.filter(x -> x.getType().equals(type))
-				.collect(Collectors.toList());
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	}
 }
